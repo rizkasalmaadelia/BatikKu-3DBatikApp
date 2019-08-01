247a179cffc5ddbee23a7a35abfca916e76a01fb
rizka salma adelia <rizkasalmaadelia@gmail.com>
2019-08-01 07:13:27 +0700

    button daerah

---
+++ app/src/main/java/com/rizkasalma/myviewpager/JatengActivity.java
@@ -0,0 +1,29 @@
+package com.rizkasalma.myviewpager;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class JatengActivity extends AppCompatActivity {
+    ImageButton buttonBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jateng);
+
+        buttonBack = findViewById(R.id.buttonBack);
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/rizkasalma/myviewpager/MainActivity.java
+++ app/src/main/java/com/rizkasalma/myviewpager/MainActivity.java
@@ -1,14 +1,24 @@
 package com.rizkasalma.myviewpager;
 
+import android.R.anim;
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+
 
 public class MainActivity extends AppCompatActivity {
     ViewPager viewPager;
+    ImageView imageViewJateng;
     TabLayout tabIndicator;
-    int images[] = {R.drawable.banner1, R.drawable.banner1, R.drawable.banner1, R.drawable.banner1};
+    int images[] = {R.drawable.banner12, R.drawable.banner12, R.drawable.banner2, R.drawable.banner12};
     MyCustomPagerAdapter myCustomPagerAdapter;
 
 
@@ -18,10 +28,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         viewPager = (ViewPager)findViewById(R.id.viewPager);
-        tabIndicator = findViewById(R.id.tab_indicator);
 
+        imageViewJateng = findViewById(R.id.image1);
         myCustomPagerAdapter = new MyCustomPagerAdapter(MainActivity.this, images);
         viewPager.setAdapter(myCustomPagerAdapter);
+        tabIndicator = findViewById(R.id.tab_indicator);
         tabIndicator.setupWithViewPager(viewPager);
+
+
+        imageViewJateng.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(getApplicationContext(), JatengActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
